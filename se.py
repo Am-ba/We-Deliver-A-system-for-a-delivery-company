df4d0a2d54b7f9a67eafd1b3b4839217ce482e0d
Amir-Baddour <amirbaddour@users.noreply.github.com>
2024-07-16 12:54:29 +0300

    Display Main/Driver/Cities Menu

--- se.py
+++ se.py
@@ -1,47 +1,63 @@
-# Main Menu
-# Display the welcome message
-# Prompt the user to enter:
-# 1. For the drivers' menu
-# 2. For the cities' menu
-# 3. To exit the system
-
-# If user selects option 1, go to Drivers' Menu
-# If user selects option 2, go to Cities' Menu
-# If user selects option 3, exit the system
-
-# Drivers' Menu
-# Display the drivers' menu options
-# Prompt the user to enter:
-# 1. To view all the drivers
-# 2. To add a driver
-# 3. To go back to the main menu
-
-# If user selects option 1, display all drivers and their details
-# If user selects option 2, prompt the user to enter the name and start city of the driver
-#    - Validate if the start city is in the database
-#    - If not, ask the user if they want to add it to the database
-#    - Generate a unique worker ID for the new driver and save the details
-# If user selects option 3, return to the main menu
-
-# Cities' Menu
-# Display the cities' menu options
-# Prompt the user to enter:
-# 1. To show all cities
-# 2. To print neighboring cities
-# 3. To print drivers delivering to a city
-
-# If user selects option 1, display the list of all cities
-# If user selects option 2, prompt the user to enter a city name
-#    - Display all cities that can be reached from the entered city
-# If user selects option 3, prompt the user to enter a city name
-#    - Display all drivers that are delivering to the entered city
-
-# Helper Functions
-# Create a function to generate a unique worker ID
-# Create a function to validate if a city is in the database
-# Create a function to add a new city to the database
-# Create a function to add a new driver to the database
-# Create a function to display all drivers
-# Create a function to display all cities
-# Create a function to find neighboring cities using BFS or DFS
-# Create a function to find drivers delivering to a  city
+class WeDeliverSystem:
+    #constructor
+    def __init__(self):
+        self.drivers = {}
+        self.cities = {}
+        self.driver_id_counter = 1
+    #Main menu
+    def main_menu(self):
+        while True:
+            print("Hello! Please enter:")
+            print("1. To go to the drivers' menu")
+            print("2. To go to the cities' menu")
+            print("3. To exit the system")
+            choice = input("Enter your choice: ")
+            if choice == '1':
+                self.drivers_menu()
+            elif choice == '2':
+                self.cities_menu()
+            elif choice == '3':
+                print("Exiting the system. Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+    #driver menu
+    def drivers_menu(self):
+        while True:
+            print("\nDrivers' Menu:")
+            print("1. To view all the drivers")
+            print("2. To add a driver")
+            print("3. To go back to main menu")
+            choice = input("Enter your choice: ")
+            if choice == '1':
+                self.view_all_drivers()
+            elif choice == '2':
+                self.add_driver()
+            elif choice == '3':
+                break
+            else:
+                print("Invalid choice. Please try again.")
+    #View all driver function
+    #add driver function
+    #city menu
+    def cities_menu(self):
+        while True:
+            print("\nCities' Menu:")
+            print("1. Show cities")
+            print("2. Print neighboring cities")
+            print("3. Print drivers delivering to city")
+            print("4. To go back to main menu")
+            choice = input("Enter your choice: ")
+            if choice == '1':
+                self.show_cities()
+            elif choice == '2':
+                self.print_neighboring_cities()
+            elif choice == '3':
+                self.print_drivers_delivering_to_city()
+            elif choice == '4':
+                break
+            else:
+                print("Invalid choice. Please try again.")
+        #show cities function
+        #print neighboring cities function
+        #orint driver delivering to city
